ca9ec5edf06cd317eec3a2f54efabb5319bbf383
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-10 10:54:49 +0530

    Initial commit

---
+++ Streak/routing.py
@@ -0,0 +1,8 @@
+from django.urls import re_path
+from . import consumers
+
+# We call the as_asgi() classmethod in order to get an ASGI application that will 
+# instantiate an instance of our consumer for each user-connection
+websocket_urlpatterns = [
+	re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+]
---
+++ chatbot/asgi.py
@@ -0,0 +1,46 @@
+"""
+ASGI config for chatbot project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+import Streak.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbot.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+# This root routing configuration specifies that when a connection is made to the Channels 
+# development server, the ProtocolTypeRouter will first inspect the type of connection.
+ # If it is a WebSocket connection (ws:// or wss://), the connection will be given to the 
+ # AuthMiddlewareStack.
+
+#  The AuthMiddlewareStack will populate the connection’s scope with a reference to the 
+# currently authenticated user, similar to how Django’s AuthenticationMiddleware populates 
+# the request object of a view function with the currently authenticated user. (Scopes will be discussed later in this tutorial.) 
+# Then the connection will be given to the URLRouter.
+
+# The URLRouter will examine the HTTP path of the connection to route it to a particular 
+# consumer, based on the provided url patterns.
+
+
+# application = get_asgi_application()
+#it tells channels what code to run when an HTTP request is received by the Channels server.
+application = ProtocolTypeRouter(
+	{
+		"http": get_asgi_application(),
+		"websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(Streak.routing.websocket_urlpatterns))
+            ),
+	}
+)
