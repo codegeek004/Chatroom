2d006795696ea2995cf05b2b4581e2837cec1a28
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-13 15:53:42 +0530

    Created channel using shell

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -71,11 +71,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'chatbot.wsgi.application'
 ASGI_APPLICATION = 'chatbot.asgi.application'
-CHANNELS_LAYERS = {
-    "default" : {
-        "BACKEND" : "channels_redis.core.RedisChannelLayer",
-        "CONFIG" : {
-            "hosts" : [("127.0.0.1", 6379)],
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
         },
     },
 }
