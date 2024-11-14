355058b3d7d17ef42443e9f4483fd2f661c54e9b
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-14 19:12:14 +0530

    chatroom fully functional

--- Streak/views.py
+++ Streak/views.py
@@ -2,17 +2,9 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 
 def index(request):
-	return render(request, 'index.html', {})
+	return render(request, 'index.html')
 
 def room(request, room_name):
+	print('room name', room_name)
 	return render(request, 'room.html', {'room_name':room_name})
 
-# def enter_room(request):
-# 	if request.method == 'POST':
-# 		room_name = request.POST.get("room_name")
-# 		if room_name:
-# 			return redirect(reverse('chat_room', kwargs={'room_name': room_name}))
-# 	return redirect('index')
-
-# def chat_room(request, room_name):
-# 	return render(request, chat_room.html, {'room_name': room_name})
